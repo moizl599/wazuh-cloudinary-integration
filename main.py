62193a72b1d5dd98b9e1b110a3bd4e06ea49df5a
Zoim <42458629+moizl599@users.noreply.github.com>
2023-09-26 13:28:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,100 +1,77 @@
-# Import env file module
-# ==============================
-from dotenv import load_dotenv
-load_dotenv()
-# Import the Cloudinary libraries
-# ==============================
-import cloudinary
-import cloudinary.uploader
-import cloudinary.api
-
-# Import to format the JSON responses
-# ==============================
 import json
 import os
-# Set your Cloudinary credentials
-# ==============================
-config = cloudinary.config(secure=True)
-
+from configparser import ConfigParser
+import cloudinary.api
 
 class API_calls:
-    def __init__(self):
-        logfile = "logfile.log"
-        reported_asset_list = '/opt/wazuh_logging/cloudinary/reported_asset_list.txt'
-        test_logfile = '/opt/wazuh_logging/cloudinary/generic_log_file.log'
-        resource_types = ['image', 'video', 'raw']
-        # API call to cloudinary for pulling the resources
-        self.logfile = logfile
-        self.reported_asset_list = reported_asset_list
-        self.resource_types = resource_types
-        self.test_logfile = test_logfile
+    """
+    A class to make API calls to Cloudinary.
+    """
+
+    def __init__(self, config_file: str):
+        """
+        Initializes the API_calls object with configurations from a file.
+        
+        :param config_file: Path to the configuration file containing necessary parameters.
+        """
+        # Loading configurations from the configuration file
+        config = ConfigParser()
+        config.read(config_file)
+        
+        # Assigning configurations to instance variables
+        self.logfile = config.get('Cloudinary', 'logfile')
+        self.reported_asset_list = config.get('Cloudinary', 'reported_asset_list')
+        self.test_logfile = config.get('Cloudinary', 'test_logfile')
+        self.resource_types = config.get('Cloudinary', 'resource_types').split(',')
 
     def project_fall_back(self):
-        # function to make sure the generic log file is always there
-        test_logfile = self.test_logfile
-        if os.path.isfile(test_logfile):
-            print("fairenough")
-        else:
-            with open(test_logfile, 'x') as f:
-                print("all good now pheww")
+        """
+        Checks for the existence of the test logfile, creates it if not found.
+        """
+        if not os.path.isfile(self.test_logfile):
+            with open(self.test_logfile, 'x') as f:
+                print("All good now, pheww!")
 
     def call_cloudinary(self):
-        # Declaring log files
-        reported_asset_list = self.reported_asset_list
-        resource_types = self.resource_types
-        test_logfile = self.test_logfile
-
-        for resource_type in resource_types:
-            # removing the log file to make sure new logs are written on the file.
-            logfile = f"/opt/wazuh_logging/cloudinary/{resource_type}_{self.logfile}"
+        """
+        Calls the Cloudinary API to pull resources and logs the results.
+        """
+        for resource_type in self.resource_types:
+            logfile = f"{self.logfile}_{resource_type}.log"
+            # Removing the logfile to write new logs
             if os.path.isfile(logfile):
                 os.remove(logfile)
-            result = json.loads(json.dumps(cloudinary.api.resources_by_moderation('perception_point', 'pending',
-                                                                                  resource_type=resource_type)))
-            data_list = result["resources"]  # data pulled from cloudinary api sits under the resources key as a list
 
-            # if the asset list exists then pull the asset list read from it and make sure that the logs are not reported again
-            if os.path.isfile(reported_asset_list):
-                asset_list = []
-                with open(reported_asset_list, "r") as reported_asset:
-                    asset = reported_asset.readlines()
-                for line in asset:
-                    asset_list.append(line.replace("\n", ""))
-                # print(asset_list)
-                with open(logfile, 'x') as logs_file:
-                    for data in data_list:
-                        if data["asset_id"] not in asset_list:
-                            with open(logfile, 'a') as write_file:
-                                data['Log_type'] = 'Cloudninary'
-                                json.dump(data, write_file)
-                                write_file.write('\n')
-                            with open(test_logfile, 'a') as write_file:
-                                data['Log_type'] = 'Cloudninary'
-                                json.dump(data, write_file)
-                                write_file.write('\n')
-                            with open(reported_asset_list, 'a') as write_file:
-                                write_file.write(data["asset_id"] + "\n")
-            else:
-                # else condition to execute the logs for the first time in case the asset list does not exists
-                with open(logfile, 'x') as logs_file:
-                    with open(reported_asset_list, 'x') as reported_asset:
-                        for data in data_list:
-                            with open(logfile, 'a') as write_file:
-                                data['Log_type'] = 'Cloudninary'
-                                json.dump(data, logs_file)
-                                write_file.write('\n')
-                            with open(test_logfile, 'a') as write_file:
-                                data['Log_type'] = 'Cloudninary'
-                                json.dump(data, write_file)
-                                write_file.write('\n')
-                            with open(reported_asset_list, 'a') as write_file:
-                                write_file.write(data["asset_id"] + "\n")
+            # Making an API call to Cloudinary
+            result = cloudinary.api.resources_by_moderation('perception_point', 'pending', resource_type=resource_type)
+            data_list = result["resources"]
+
+            # Reading already reported assets from the file
+            asset_list = []
+            if os.path.isfile(self.reported_asset_list):
+                with open(self.reported_asset_list, "r") as reported_asset:
+                    asset_list = [line.strip() for line in reported_asset.readlines()]
+
+            # Writing logs and updating the reported assets list
+            with open(logfile, 'a') as logs_file, open(self.test_logfile, 'a') as test_file, open(self.reported_asset_list, 'a') as reported_asset:
+                for data in data_list:
+                    if data["asset_id"] not in asset_list:
+                        data['Log_type'] = 'Cloudinary'
+                        json.dump(data, logs_file)
+                        logs_file.write('\n')
+                        json.dump(data, test_file)
+                        test_file.write('\n')
+                        reported_asset.write(data["asset_id"] + "\n")
 
 
 def main():
-    lets_run_it = API_calls()
+    """
+    The main function to run the API calls and associated methods.
+    """
+    lets_run_it = API_calls('./key.cfg')
     lets_run_it.project_fall_back()
     lets_run_it.call_cloudinary()
 
 
-main()
+if __name__ == "__main__":
+    main()
